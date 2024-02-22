fe9c1e7077a6d0ceaa51280cf7cd74193b75dc7d
Katarina Gresova <514001@mail.muni.cz>
2024-02-22 17:11:30 +0100

    script to setup conda env

---
+++ prepare_conda.sh
@@ -0,0 +1,12 @@
+ENV_NAME="bench"
+	
+conda create -n $ENV_NAME python=3.8 ipykernel nb_conda_kernels
+source activate $ENV_NAME
+
+pip install numpy	
+pip install pandas
+pip install tensorflow
+pip install matplotlib
+pip install seaborn
+pip install scikit-learn
+pip install scipy
