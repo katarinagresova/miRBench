14ff4997cc5c431752618e3eaaf803ab55d0e281
Katarina Gresova <514001@mail.muni.cz>
2024-02-26 16:52:17 +0100

    downloading hejret cnn if not present

--- tools/hejret_mirna_cnn.py
+++ tools/hejret_mirna_cnn.py
@@ -1,5 +1,6 @@
 import argparse
-import numpy as np
+import os
+import urllib.request 
 import pandas as pd
 from tensorflow import keras as k
 
@@ -10,19 +11,26 @@ def predict_probs(df, miRNA_col, gene_col, model):
     fun predicts the probability of miRNA:target site binding in df file
     :param df: input dataframe with sequences containing 'gene' and 'noncodingRNA' columns
     :param model: Keras model used for predicting
-    :param output: output file to write probabilities to
     """
-    #gene_length = 50
-
-    #orig_len = len(df)
-    #df = df[df["gene"].str.len() == gene_length]
-    #processed_len = len(df)
-
-    #if orig_len != processed_len:
-    #    print("Skipping " + str(orig_len - processed_len) + " pairs due to inappropriate target length.")
 
     ohe = one_hot_encoding(df, miRNA_col, gene_col)
     return model.predict(ohe)
+
+def get_model_path():
+    current_path = os.path.realpath(__file__)
+    model_dir_path = os.path.join(os.path.dirname(current_path), '../models/Hejret_miRNA_CNN')
+    if not os.path.exists(model_dir_path):
+        os.mkdir(model_dir_path)
+
+    model_path = os.path.join(model_dir_path, 'model_miRNA.h5')
+    if os.path.exists(model_path):
+        return model_path
+
+    print('Downloading the model...')
+    url = 'https://github.com/ML-Bioinfo-CEITEC/HybriDetector/raw/main/ML/Models/model_miRNA.h5'
+    urllib.request.urlretrieve(url, model_path)
+
+    return model_path
 
 if __name__ == '__main__':
     # Parse command line arguments
@@ -35,15 +43,15 @@ if __name__ == '__main__':
     parser.add_argument('--gene_column', type=str, help='Name of the column containing gene sequences')
     # Path to the output file
     parser.add_argument('--output', type=str, help='Path to the output file')
-    # Path to the trained model
-    parser.add_argument('--model', type=str, help='Path to the trained model')
     # Parse the arguments
     args = parser.parse_args()
 
     # Read the input file
     data = pd.read_csv(args.input, sep='\t')
 
-    model = k.models.load_model(args.model)
+    model_path = get_model_path()
+    model = k.models.load_model(model_path)
+
     preds = predict_probs(data, args.miRNA_column, args.gene_column, model)
     data['hejret_mirna'] = preds
     data.to_csv(args.output, sep='\t', index=False)
