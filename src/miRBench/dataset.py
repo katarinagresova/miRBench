997ca679b6fb9f0b5549468bec2ce03b151a1fd3
Katarina Gresova <514001@mail.muni.cz>
2024-08-02 11:16:38 +0200

    download from zenodo

--- src/miRBench/dataset.py
+++ src/miRBench/dataset.py
@@ -5,9 +5,42 @@ from pathlib import Path
 CACHE_PATH = Path.home() / ".miRBench" / "datasets"
 DATASET_FILE = "dataset.tsv"
 
-def list_datasets():
-    # TODO: add more information about datasets
-    return ["Hejret2023"]
+def list_datasets(full=False):
+
+    datasets = {
+        "AGO2_CLASH_Hejret2023": {
+            "splits": {
+                "train": {
+                    "ratios": ["10"]
+                },
+                "test": {
+                    "ratios": ["1", "10", "100"]
+                }
+            }
+        },
+        "AGO2_eCLIP_Klimentova2022": {
+            "splits": {
+                "test": {
+                    "ratios": ["1", "10", "100"]
+                }
+            }
+        },
+        "AGO2_eCLIP_Manakov2022": {
+            "splits": {
+                "train": {
+                    "ratios": ["1", "10", "100"]
+                },
+                "test": {
+                    "ratios": ["1", "10", "100"]
+                }
+            }
+        }
+    }
+
+    if full:
+        return datasets
+    else:
+        return list(datasets.keys())
 
 def get_dataset(dataset_name, ratio, split, force_download = False):
     """
@@ -16,24 +49,35 @@ def get_dataset(dataset_name, ratio, split, force_download = False):
 
     local_path = Path(CACHE_PATH / dataset_name / ratio / split / DATASET_FILE)
     if not local_path.exists() or force_download:
+        print(f"Downloading {dataset_name} dataset, ratio {ratio}, split {split}")
         download_dataset(dataset_name, local_path, ratio, split)
 
     dataset = pd.read_csv(local_path, sep="\t")
     return dataset
 
+def get_dataset_path(dataset_name, ratio, split, force_download = False):
+    """
+    Get dataset path from cache or download it if not present
+    """
+
+    local_path = Path(CACHE_PATH / dataset_name / ratio / split / DATASET_FILE)
+    if not local_path.exists() or force_download:
+        print(f"Downloading {dataset_name} dataset, ratio {ratio}, split {split}")
+        download_dataset(dataset_name, local_path, ratio, split)
+
+    return local_path
+
 def download_dataset(dataset_name, download_path, ratio, split):
 
-    # TODO add verification that combination of name, split and ratio is valid
-    if dataset_name == "Helwak2013":
-        url = f"https://github.com/ML-Bioinfo-CEITEC/miRBind/raw/main/Datasets/{split}_set_1_{ratio}_CLASH2013_paper.tsv"
-    elif dataset_name == "Hejret2023":
-        url = f"https://github.com/ML-Bioinfo-CEITEC/HybriDetector/raw/main/ML/Datasets/miRNA_{split}_set_{ratio}.tsv"
-    elif dataset_name == "Klimentova2022":
-        raise NotImplementedError("Klimentova dataset is not available for download")
-    elif dataset_name == "Manakov2022":
-        raise NotImplementedError("Manakov dataset is not available for download")
-    else:
-        raise ValueError("Unknown dataset name")
+    available_datasets = list_datasets(full=True)
+    if dataset_name not in available_datasets:
+        raise ValueError(f"Dataset {dataset_name} not found")
+    if split not in available_datasets[dataset_name]["splits"]:
+        raise ValueError(f"Split {split} not found for dataset {dataset_name}")
+    if ratio not in available_datasets[dataset_name]["splits"][split]["ratios"]:
+        raise ValueError(f"Ratio {ratio} not found for split {split} of dataset {dataset_name}")
+
+    url = f'https://zenodo.org/records/11657638/files/{dataset_name}_{ratio}_{split}_dataset.tsv?download=1'
     
     data_dir = download_path.parent
     if not data_dir.exists():
