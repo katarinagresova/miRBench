40f3a36f042ee71df6ad6a0d816e469c207e8a04
Katarina Gresova <514001@mail.muni.cz>
2024-10-14 15:08:05 +0200

    increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'miRBench',
-    version = '0.1.1',
+    version = '1.0.0',
     description="A collection of datasets and predictors for benchmarking miRNA target site prediction algorithms",
     long_description=long_description,
     long_description_content_type="text/markdown",
